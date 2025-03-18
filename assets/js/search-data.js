cb4183cba3475663a3cb20f39c47f0c4a5751507
mahtabfaraji1 <45668993+mahtabfaraji1@users.noreply.github.com>
2025-03-18 16:08:32 +0000

    Deploying to gh-pages from @ mahtabfaraji1/mahtabfaraji1.github.io@4292acff93a7d759544cf31436c18b70c8c408bb 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "projects",
-          description: "A growing collection of your cool projects.",
+          description: "Here are a summary of my projects.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
