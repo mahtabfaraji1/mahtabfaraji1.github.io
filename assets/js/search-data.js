07df310cbdc5e47325e8cf13dec262e19fe373ae
mahtabfaraji1 <45668993+mahtabfaraji1@users.noreply.github.com>
2025-03-25 14:23:26 +0000

    Deploying to gh-pages from @ mahtabfaraji1/mahtabfaraji1.github.io@d015fa5921b4c0300ce5fdeeb99df6820cee17cb 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -390,7 +390,7 @@ ninja.data = [{
           description: "",
           section: "News",},{id: "projects-iris-and-pupil-detection-in-cataract-surgical-data",
           title: 'Iris and Pupil Detection in Cataract Surgical Data',
-          description: "Object detection in cataract surgery images using YOLO to monitor intraoperative deviations.",
+          description: "Object detection in cataract surgery images using YOLOv5 to monitor intraoperative deviations.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
